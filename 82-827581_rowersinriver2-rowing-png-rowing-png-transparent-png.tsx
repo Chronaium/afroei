61995c5698922826b6f251cf30b753bc02c32e71
Fynn van den Berg <Chronaium@users.noreply.github.com>
2021-03-08 15:59:45 +0100

    d

--- 82-827581_rowersinriver2-rowing-png-rowing-png-transparent-png.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="82-827581_rowersinriver2-rowing-png-rowing-png-transparent-png" tilewidth="32" tileheight="32" tilecount="351" columns="39">
- <image source="82-827581_rowersinriver2-rowing-png-rowing-png-transparent-png.png" trans="ff00ff" width="1274" height="300"/>
-</tileset>
